fc8ffb27d1a774fc4f9b7d1cc17aa198dc3e6498
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2025-12-30 17:09:01 -0600

    tadah

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   skipMiddlewareUrlNormalize: true,
   experimental: {
