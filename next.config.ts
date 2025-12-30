a621114a48fe0e135874ac6aa9fc6d13a1e27056
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2025-12-30 17:06:27 -0600

    more tries

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   skipMiddlewareUrlNormalize: true,
+  experimental: {
+    trustHostHeader: true,
+  }
 };
 
 export default nextConfig;
